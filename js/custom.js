0c40effb851edabc582c252a157d5484e71106b7
Chad Uhl <chaduhl@ku.edu>
2019-08-19 18:09:33 -0500

    Updated nav menu

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function customDropdown() {
+  var x = document.getElementById("mobile-nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
